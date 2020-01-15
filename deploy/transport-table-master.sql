c478e9c5842477793eaf7974c9f7b0450ff311ba
nurlailifajriyah <nurlaili.fajriyah@gmail.com>
2020-01-15 21:30:32 +0700

    change id field name

--- deploy/transport-table-master.sql
+++ deploy/transport-table-master.sql
@@ -2,10 +2,10 @@
 
 BEGIN;
 
-CREATE TABLE transport.product_type (id INT PRIMARY KEY, product_type_name TEXT);
-CREATE TABLE transport.supplier (id INT PRIMARY KEY, supplier_name TEXT, supplier_type TEXT);
-CREATE TABLE transport.product (id INT PRIMARY KEY, product_name TEXT, product_type_id INT REFERENCES transport.product_type(id), supplier_id INT REFERENCES transport.supplier(id));
-CREATE TABLE transport.user (id INT PRIMARY KEY, user_name TEXT);
-CREATE TABLE transport.auth_type (id INT PRIMARY KEY, auth_type TEXT, auth_provider TEXT);
+CREATE TABLE transport.product_type (product_type_id INT PRIMARY KEY, product_type_name TEXT);
+CREATE TABLE transport.supplier (supplier_id INT PRIMARY KEY, supplier_name TEXT, supplier_type TEXT);
+CREATE TABLE transport.product (product_id INT PRIMARY KEY, product_name TEXT, product_type_id INT REFERENCES transport.product_type(product_type_id), supplier_id INT REFERENCES transport.supplier(supplier_id));
+CREATE TABLE transport.user (user_id INT PRIMARY KEY, user_name TEXT);
+CREATE TABLE transport.auth_type (auth_type_id INT PRIMARY KEY, auth_type TEXT, auth_provider TEXT);
 
 COMMIT;
--- deploy/transport-table-trx.sql
+++ deploy/transport-table-trx.sql
@@ -2,11 +2,11 @@
 
 BEGIN;
 
-CREATE TABLE transport.order (id BIGINT PRIMARY KEY, total_amount MONEY, order_status TEXT, user_id INT REFERENCES transport.user(id));
-CREATE TABLE transport.trip (id INT PRIMARY KEY, trip_from TEXT, trip_to TEXT, amount MONEY, product_id INT REFERENCES transport.product(id), order_id BIGINT REFERENCES transport.order(id));
-CREATE TABLE transport.route (id INT PRIMARY KEY, route_from TEXT, route_to TEXT, start_time TIME, end_time TIME, trip_id INT REFERENCES transport.trip(id));
-CREATE TABLE transport.segment (id INT PRIMARY KEY, segment_from TEXT, segment_to TEXT, start_time TIME, end_time TIME);
-CREATE TABLE transport.passenger (id INT PRIMARY KEY, order_id BIGINT REFERENCES transport.order(id), passenger_name TEXT, age SMALLINT);
+CREATE TABLE transport.order (order_id BIGINT PRIMARY KEY, total_amount MONEY, order_status TEXT, user_id INT REFERENCES transport.user(user_id));
+CREATE TABLE transport.trip (trip_id INT PRIMARY KEY, trip_from TEXT, trip_to TEXT, amount MONEY, product_id INT REFERENCES transport.product(product_id), order_id BIGINT REFERENCES transport.order(order_id));
+CREATE TABLE transport.route (route_id INT PRIMARY KEY, route_from TEXT, route_to TEXT, start_time TIME, end_time TIME, trip_id INT REFERENCES transport.trip(trip_id));
+CREATE TABLE transport.segment (segment_id INT PRIMARY KEY, segment_from TEXT, segment_to TEXT, start_time TIME, end_time TIME);
+CREATE TABLE transport.passenger (passenger_id INT PRIMARY KEY, order_id BIGINT REFERENCES transport.order(order_id), passenger_name TEXT, age SMALLINT);
 
 COMMIT;
 
